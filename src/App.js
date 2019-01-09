0d4849c8e451db095f40aff4e5ebaa91b13613bf
Celia Marshall <celia.m@gmail.com>
2019-01-09 13:06:26 -0800

    change routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import Page from './Page'
+import SpecificPage from './SpecificPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route path='/page/:id' component={SpecificPage} />
+            <Route path='/page' component={Page} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/Page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Page = props => 
+  <div>
+    <h1>Welcome To the page</h1>
+    <Link to='/'>Home</Link>
+  </div>
+
+export default Page
